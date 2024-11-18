592abc1afd930284598f2ab54dd94610fa26582c
jaironf <jaironf.94@gmail.com>
2024-11-18 21:12:43 +0100

    ThemeDarkMode eliminado

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import { Button } from "@nextui-org/react";
-import Image from "next/image";
-import { ThemeSwitcher } from "./components/ThemeSwitcher";
 
 export default function Home() {
   return (
     <main >
       <p>Movie app</p>
       <Button color="primary">Click Me</Button>
-      <ThemeSwitcher />
     </main>
   );
 }
